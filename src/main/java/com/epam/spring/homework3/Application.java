86424d731cd8e48d236d69bc584a43b73d70ceab
Vasil Petrus <vasil.petrus@gmail.com>
2021-11-11 07:55:27 +0200

    HW_3 completed

--- src/main/java/com/epam/spring/homework3/Application.java
+++ src/main/java/com/epam/spring/homework3/Application.java
@@ -1,5 +1,6 @@
 package com.epam.spring.homework3;
 
+import com.epam.spring.homework3.beans.ExampleBean;
 import com.epam.spring.homework3.config.BeansConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,10 +11,12 @@ public class Application {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
 
         // Перелік усіх бінів, що створені у ApplicationContext, та їх конфігурація.
-        System.out.println("//////////////////////////////////////////////////////////////////////////");
+        System.out.println("////////////////////////// ALL BEANS ////////////////////////////////////");
         String[] beansNames = context.getBeanDefinitionNames();
         for (String bean : beansNames) {
+            if (context.getBean(bean) instanceof ExampleBean) {
                 System.out.println(context.getBean(bean).toString());
+            }
         }
         System.out.println("//////////////////////////////////////////////////////////////////////////");
 
